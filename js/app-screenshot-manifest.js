60be6bf215a3dd533b14782be11e9a3bdd784ae4
Antonio J. Martinez <martinez.sanchez.aj@gmail.com>
2026-04-27 15:57:32 +0200

    new apps, new design

--- js/app-screenshot-manifest.js
+++ js/app-screenshot-manifest.js
@@ -5,6 +5,19 @@ window.APP_SCREENSHOT_MANIFEST = {
     "images/apps/screenshots/solora/03.jpg",
     "images/apps/screenshots/solora/04.jpg"
   ],
+  "6756927629": [
+    "images/apps/screenshots/fit3/01.jpg",
+    "images/apps/screenshots/fit3/02.jpg",
+    "images/apps/screenshots/fit3/03.jpg",
+    "images/apps/screenshots/fit3/04.jpg"
+  ],
+  "6760191862": [
+    "images/apps/screenshots/podpin/01.jpg",
+    "images/apps/screenshots/podpin/02.jpg",
+    "images/apps/screenshots/podpin/03.jpg",
+    "images/apps/screenshots/podpin/04.jpg",
+    "images/apps/screenshots/podpin/05.jpg"
+  ],
   "6752911194": [
     "images/apps/screenshots/cogno/01.jpg",
     "images/apps/screenshots/cogno/02.jpg",
